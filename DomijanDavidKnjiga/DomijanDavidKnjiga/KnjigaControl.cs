d71659c4feeabff1970f8c00d2a08e45a0cf8ed2
DavidDomijan <36442082+DavidDomijan@users.noreply.github.com>
2018-04-17 17:57:12 +0200

    Add files via upload

---
+++ DomijanDavidKnjiga/DomijanDavidKnjiga/KnjigaControl.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomijanDavidKnjiga
+{
+    class KnjigaControl
+    {
+        List<Poglavlje> popisPoglavlja = new List<Poglavlje>();
+        int max = 0;
+        int min = 0;
+        public void dodajPoglavlje(Poglavlje objekt)
+        {
+            Poglavlje poglavlje = new Poglavlje(objekt.getNazivPoglavlja(),objekt.getUkupanBrojStranica());
+           
+        }
+        public string ispisiPoglavlja() {
+            string ispis = "Poglavlja knjige: \n";
+            foreach (Poglavlje poglavlje in popisPoglavlja)
+            {
+                ispis = ispis + "Naziv poglavlja: " + poglavlje.getNazivPoglavlja()+
+                    "\nBroj Stranica: " + poglavlje.getUkupanBrojStranica();
+            }
+            return ispis;
+        }
+        public string ispisiUkupanBrojStranica() {
+            int total = 0;
+            foreach (Poglavlje poglavlje in popisPoglavlja)
+            {
+                total = total + poglavlje.getBrojStranice();
+            }
+            return "Ukupan broj stranica knjige je: " + total;
+        }
+        public string ispisiNajvecePoglavlje() {
+            
+            string najvecePoglavlje="";
+            foreach (Poglavlje poglavlje in popisPoglavlja)
+            {
+                if (poglavlje.getBrojStranice() > max) {
+                    max = poglavlje.getBrojStranice();
+                    najvecePoglavlje = poglavlje.getNazivPoglavlja();
+                }
+            }
+            return "Najvece poglavlje je: " + najvecePoglavlje;
+        }
+        public string ispisiNajmanjePoglavlje()
+        {
+            max = 0;
+            foreach (Poglavlje poglavlje in popisPoglavlja)
+            {
+                
+                if (poglavlje.getBrojStranice() > max)
+                {
+                    max = poglavlje.getBrojStranice();
+                    min = max;
+                }
+                
+            }
+
+            string najmanjePoglavlje = "";
+            foreach (Poglavlje poglavlje in popisPoglavlja)
+            {
+                if (poglavlje.getBrojStranice() <= min)
+                {
+                    min = poglavlje.getBrojStranice();
+                    najmanjePoglavlje = poglavlje.getNazivPoglavlja();
+                }
+            }
+            return "Najmanje poglavlje je: " + najmanjePoglavlje;
+        }
+    }
+}
---
+++ DomijanDavidKnjiga/DomijanDavidKnjiga/Poglavlje.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomijanDavidKnjiga
+{
+    class Poglavlje
+    {
+        private string nazivPoglavlja;
+        private int brojStranice,ukupanBrojStranica;
+
+        public void setNazivPoglavlja(string imePoglavlja) {
+            nazivPoglavlja = imePoglavlja;
+        }
+
+        public void setBrojStranice(int brojStranice)
+        {
+            this.brojStranice = brojStranice;
+        }
+
+        public void setUkupanBrojStranice(int ukupanBrojStranica)
+        {
+            this.ukupanBrojStranica = ukupanBrojStranica;
+        }
+
+        public string getNazivPoglavlja() {
+
+            return nazivPoglavlja;
+        }
+        public int getBrojStranice() {
+
+            return brojStranice;
+        }
+        public int getUkupanBrojStranica() {
+
+            return ukupanBrojStranica;
+        }
+        //konstruktor
+        public Poglavlje(string naziv, int broj) {
+            nazivPoglavlja = naziv;
+            brojStranice = broj;
+        }
+        public Poglavlje() {
+
+        }
+        //
+    }
+}
